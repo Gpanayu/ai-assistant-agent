5ff80fbc7c115587960c98a85b18b73eef4f3800
GryphNdor <griffin@ogura-family.com>
2025-01-23 21:10:20 -0500

    add playground, ui updates

--- apps/api/src/main.py
+++ apps/api/src/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
 
 class InputBody(BaseModel):
     code: str
+    channel: str
 
 
 class SocketManager:
@@ -32,6 +33,12 @@ class SocketManager:
         await ws.accept()
         ws.id = id
         self.connections.append(ws)
+        if len(self.connections) == 1:
+            """
+            TODO: this would be where we configure the initial code
+            """
+            msg = json.dumps({"event": "initial", "payload": "# Collaborative Editor"})
+            await self.broadcast(msg)
 
     def disconnect(self, ws: WebSocket):
         self.connections.remove(ws)
@@ -40,6 +47,11 @@ class SocketManager:
     async def broadcast(self, msg: str):
         for ws in self.connections:
             await ws.send_text(msg)
+
+    async def direct_message(self, msg: str, id: str):
+        for ws in self.connections:
+            if ws.id == id:
+                await ws.send_text(msg)
 
 
 socketManager = SocketManager()
@@ -53,6 +65,7 @@ async def test(rawCode: InputBody):
     sys.stderr = buffer
 
     # run the code
+    # probably should implement this later: https://restrictedpython.readthedocs.io/en/latest/
     try:
         exec(rawCode.code)
     except Exception as err:
@@ -60,9 +73,16 @@ async def test(rawCode: InputBody):
 
     sys.stdout = sys.__stdout__
 
-    event = {"event": "run", "stdout": buffer.getvalue()}
+    event = {
+        "event": "run",
+        "stdout": buffer.getvalue(),
+        "all": rawCode.channel == "all",
+    }
 
-    await socketManager.broadcast(json.dumps(event))
+    if rawCode.channel == "all":
+        await socketManager.broadcast(json.dumps(event))
+    else:
+        await socketManager.direct_message(json.dumps(event), rawCode.channel)
 
     return Response(content=buffer.getvalue(), media_type="text/plain")
 
@@ -72,7 +92,9 @@ async def websocket_endpoint(websocket: WebSocket, id: str):
     await socketManager.connect(websocket, id)
     try:
         while True:
-            await websocket.receive_text()
+            data = await websocket.receive_text()
+            loaded = json.loads(data)
+            print(loaded["payload"])
 
     except WebSocketDisconnect:
         socketManager.disconnect(websocket)
--- apps/editor/src/main.ts
+++ apps/editor/src/main.ts
@@ -9,9 +9,9 @@ import "./assets/styles.css"
 import * as Y from 'yjs'
 import { yCollab } from 'y-codemirror.next'
 import { WebrtcProvider } from 'y-webrtc'
+import { YText } from "yjs/dist/src/internals"
 
 const id = Math.floor(Math.random() * 1e9).toString(36)
-
 const ws = new WebSocket(`ws://127.0.0.1:8000/ws/${id}`);
 
 ws.addEventListener("open", (_) => {
@@ -20,32 +20,64 @@ ws.addEventListener("open", (_) => {
 
 ws.addEventListener("message", (event) => {
   const data = JSON.parse(event.data)
-
   if (data["event"] === "run") {
-    console.log("ran")
-    appendToHistory(data["stdout"])
+    console.log(data)
+    appendToHistory(data["stdout"], data["all"])
+  }
+  if (data["event"] === "initial") {
+    ytext.insert(0, data["payload"])
   }
 })
+
+export const userColors = [
+  { color: '#30bced', light: '#30bced33' },
+  { color: '#6eeb83', light: '#6eeb8333' },
+  { color: '#ffbc42', light: '#ffbc4233' },
+  { color: '#ecd444', light: '#ecd44433' },
+  { color: '#ee6352', light: '#ee635233' },
+  { color: '#9ac2c9', light: '#9ac2c933' },
+  { color: '#8acb88', light: '#8acb8833' },
+  { color: '#1be7ff', light: '#1be7ff33' }
+]
+
+export const color = userColors[Math.floor(Math.random() * 8) % userColors.length]
 
 const ydoc = new Y.Doc()
 const provider = new WebrtcProvider('prime-collab-room', ydoc)
 const ytext = ydoc.getText('codemirror')
 const undoManager = new Y.UndoManager(ytext)
 
-ydoc.on('update', _ => {
-  console.log(ytext.toString())
-})
-
 provider.awareness.setLocalStateField('user', {
-  name: 'Anonymous ' + Math.floor(Math.random() * 100),
-  color: '#30bced',
-  colorLight: '#30bced33'
+  name: id,
+  color: color.color,
+  colorLight: color.light
 })
 
-new EditorView({
+undoManager.on('stack-item-added', event => {
+  event.stackItem.meta.set('cursor-location', mainView.state.selection.main.head)
+  event.stackItem.meta.set('user-id', provider.awareness.getLocalState().user.name)
+  console.log("undoManager fired")
+
+  let payload = {
+    cursor: mainView.state.selection.main.head,
+    doc: mainView.state.doc.toString(),
+    name: provider.awareness.getLocalState().user.name
+  }
+
+  ws.send(JSON.stringify({event: "update", payload: payload}))
+})
+
+
+let mainView = new EditorView({
   doc: ytext.toString(),
   extensions: [basicSetup, python(), keymap.of([indentWithTab]), oneDark, yCollab(ytext, provider.awareness, { undoManager })],
   parent: document.querySelector<HTMLDivElement>("#editor")!
+})
+
+let secondaryView = new EditorView({
+  doc: "# Personal Playground\n# Code will not be shared with others\n\nprint('hello playground')",
+  extensions: [basicSetup, python(), keymap.of([indentWithTab]), oneDark],
+  parent: document.querySelector<HTMLDivElement>("#secondary")!
 })
 
 
@@ -60,6 +92,7 @@ new EditorView({
 document.querySelector("#run")!.addEventListener("click", runCode);
 document.querySelector("#clear")!.addEventListener("click", clearCode);
 document.querySelector("#toggle")!.addEventListener("click", toggleView);
+document.querySelector("#update")!.addEventListener("click", updateName);
 
 /*
 +------------------+
@@ -68,21 +101,46 @@ document.querySelector("#toggle")!.addEventListener("click", toggleView);
 |                  |
 +------------------+
 */
-let history: [Date, string][] = []
+let history: [Date, string, boolean][] = []
+
+let tabs = document.querySelectorAll(".tab");
+
+for (let i = 0; i < tabs.length; i++) {
+  let self = tabs[i];
+  self.addEventListener('click', function() {
+    let data = this.getAttribute('data-tab');
+    document.querySelectorAll('.tab-pane.active')[0].classList.remove('active');
+    document.querySelectorAll('.tab-pane[data-pane="'+data+'"]')[0].classList.add('active');
+    document.querySelectorAll('.tab.active')[0].classList.remove('active');
+    this.classList.add('active');
+  });
+}
 
 async function runCode() {
+  const collab = document.querySelector('.tab-pane[data-pane="0"].active')
+  let channel = ""
+  let code: string | YText = ""
+
+  if (collab) {
+    channel = "all"
+    code = ytext
+  }
+  else {
+    channel = id
+    code = secondaryView.state.doc.toString()
+  }
   await fetch("http://127.0.0.1:8000/test", {
     method: "POST",
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({code: ytext}),
+    body: JSON.stringify({code: code, channel: channel})
   })
 }
 
-function appendToHistory(output: string) {
-  history.push([new Date(), output])
+function appendToHistory(output: string, all: boolean) {
+  history.push([new Date(), output, all])
 
   const outputDiv = document.querySelector<HTMLDivElement>("#output")!
   outputDiv.innerHTML = ""
@@ -100,11 +158,19 @@ function appendToHistory(output: string) {
       <p>${command[1]}</p>
       <p>${HOURS}:${MINUTES}:${SECONDS}</p>
     `
+
+    const authored = document.createElement("span")
+    authored.className = "outputLine"
+    authored.innerHTML = `<p></p>${command[2] ? '' : '<i>ran from playground</i>'}`
+    authored.style.color = "yellow"
+
     if (i % 2 === 1) {
       node.className += " active"
+      authored.className += " active"
     }
 
     outputDiv!.appendChild(node)
+    outputDiv!.appendChild(authored)
   }
 
   outputDiv.scrollTop = outputDiv.scrollHeight - outputDiv.clientHeight;
@@ -127,3 +193,12 @@ function toggleView() {
   }
 }
 
+function updateName() {
+  const input = document.querySelector<HTMLInputElement>("#nameInput")!
+  console.log(input.value)
+  provider.awareness.setLocalStateField('user', {
+    name: input.value,
+    color: color.color,
+    colorLight: color.light
+  })
+}
